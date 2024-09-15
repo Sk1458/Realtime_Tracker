fe448d1df22cb7d1d9e2e9e1e17aaf8f7cb17f23
Saikumar Voddepally <dredwing147@gmail.com>
2024-09-16 00:56:46 +0530

    Changed Directory Structure

--- Backend/app.js
+++ Backend/app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000
+const cors = require('cors');
 const socketio = require('socket.io');
 const http = require('http');
-
 const server = http.createServer(app);
 const io = socketio(server);
+
+const port = process.env.PORT || 3000
 
 server.listen(port, () => {
     console.log(`Server is runnning on port ${port}`);
 });
 
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+app.set("views", "../Frontend/views")
+app.use(express.static(path.join(__dirname, "../Frontend/public")));
 
 io.on("connection", function (socket) {
     socket.on("send-location", function (data) {
