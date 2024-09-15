a5dbcd112390f46541730eea9979e6d3578f3d8c
Saikumar Voddepally <dredwing147@gmail.com>
2024-09-15 23:54:02 +0530

    Changed Port

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const port = process.env.PORT || 3000
 const socketio = require('socket.io');
 const http = require('http');
 
 const server = http.createServer(app);
 const io = socketio(server);
 
-server.listen(3000, () => {
-    console.log("Server is runnning on port 3000");
+server.listen(port, () => {
+    console.log(`Server is runnning on port ${port}`);
 });
 
 app.set("view engine", "ejs");
